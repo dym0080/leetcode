01e61456dd0821f2d261ba07d4cf6d7f7602f559
dym0080 <308960474@qq.com>
2019-11-28 11:44:54 +0800

    update 349

--- code/349/349_intersection_of_two_arrays.cs
+++ code/349/349_intersection_of_two_arrays.cs
@@ -1,7 +1,26 @@
-public class Solution {
+public class Solution1 {
     public int[] Intersection(int[] nums1, int[] nums2) {
         var set1 = new HashSet<int>(nums1);
         set1.IntersectWith(new HashSet<int>(nums2));
         return set1.ToArray();
+    }
+}
+
+public class Solution2 {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        var arr1 = DelRepeatData(nums1);
+        var arr2 = DelRepeatData(nums2);
+        var result = new List<int>();
+        foreach(int item in arr1){
+            if(Array.IndexOf(arr2,item) > -1){
+                result.Add(item);
+            }
+        }
+        return result.ToArray();
+    }
+
+    public int[] DelRepeatData(int[] arr)
+    {
+        return arr.GroupBy(p => p).Select(p => p.Key).ToArray();
     }
 }
