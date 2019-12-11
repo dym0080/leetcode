29f8a677fb6b535a0307d70cf91dc26bdd0081c2
dym0080 <308960474@qq.com>
2019-12-11 10:34:16 +0800

    update 141

--- code/141/141_linked_list_cycle.py
+++ code/141/141_linked_list_cycle.py
@@ -3,8 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-class Solution(object):
+class Solution1(object):
     def hasCycle(self, head):
         """
         :type head: ListNode
@@ -17,4 +16,24 @@ class Solution(object):
             slow = slow.next      # 慢指针 每次走一步
             if fast == slow:      # 当慢指针和快指针相遇时就说明有环
                 return True
+        return False
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution2(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        s = set()
+        while head:
+            if head in s:
+                return True
+            s.add(head)
+            head = head.next
         return False
--- code/141/README.md
+++ code/141/README.md
@@ -8,9 +8,15 @@
 
 ## 解题思路
 
-### 1. [python](../141/141_linked_list_cycle.py) 的思路
+### 1. [python-Solution1](../141/141_linked_list_cycle.py) 的思路
 
 使用双指针方法：如果有环，快指针最终将与慢指针相遇。
+
+### 2. [python-Solution2](../141/141_linked_list_cycle.py) 的思路
+
+> 这个解决方案是看了官方的 `Java` 版本写出来的。
+
+使用哈希表法，依次循环链表往 `set` 中添加当前元素，如果遇到 `set` 已存在之前添加过的，说明有闭环，此时返回 `Ture` 退出循环。如果没有闭环，在循环结束返回 `False`。
 
 ## 提交未通过记录
 
